5f35e3d3b4be64c3187e6246d62f21837b0d99ad
Georges Biaux <georges.biaux@symeo.io>
2023-01-31 13:50:43 +0100

    Implement auth0

--- src/bootstrap/application.module.ts
+++ src/bootstrap/application.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationController } from 'src/application/controller/configuration.controller';
 import { DomainModule } from 'src/bootstrap/domain.module';
+import { AuthenticationModule } from 'src/bootstrap/authentication.module';
 
 @Module({
-  imports: [DomainModule],
+  imports: [DomainModule, AuthenticationModule],
   controllers: [ConfigurationController],
 })
 export class ApplicationModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from 'src/bootstrap/application.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { config } from 'symeo/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+  app.enableCors({
+    credentials: true,
+    origin: config.cors.origin,
+  });
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new (AuthGuard('jwt'))());
   await app.listen(3000);
 }
 bootstrap();
