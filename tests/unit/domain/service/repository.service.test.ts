39341afe7acbea6de921dc2745f58382c15b89d5
Georges Biaux <georges.biaux@symeo.io>
2023-02-03 08:07:39 +0100

    Fix tests

--- tests/unit/domain/service/repository.service.test.ts
+++ tests/unit/domain/service/repository.service.test.ts
@@ -3,8 +3,8 @@ import GithubAdapterPort from 'src/domain/port/out/github.adapter.port';
 import User from 'src/domain/model/user.model';
 import { faker } from '@faker-js/faker';
 import { VCSProvider } from 'src/domain/model/vcs-provider.enum';
-import { OrganizationService } from 'src/domain/service/organization.service';
 import { RepositoryService } from 'src/domain/service/repository.service';
+import ConfigurationStoragePort from 'src/domain/port/out/configuration.storage.port';
 
 describe('RepositoryService', () => {
   describe('getRepositories', () => {
@@ -15,21 +15,29 @@ describe('RepositoryService', () => {
         faker.internet.email(),
         VCSProvider.GitHub,
       );
-      const organizationName: string = faker.name.firstName();
+
+      const mockedConfigurationStoragePort: ConfigurationStoragePort =
+        mock<ConfigurationStoragePort>();
+      const configurationStoragePort = instance(mockedConfigurationStoragePort);
+
       const mockedGithubAdapterPort: GithubAdapterPort =
         mock<GithubAdapterPort>();
       const githubAdapterPort = instance(mockedGithubAdapterPort);
+
       const repositoryService: RepositoryService = new RepositoryService(
         githubAdapterPort,
+        configurationStoragePort,
       );
-      const spy = jest.spyOn(githubAdapterPort, 'getRepositories');
+      const spy = jest
+        .spyOn(githubAdapterPort, 'getRepositories')
+        .mockImplementation(() => Promise.resolve([]));
 
       // When
-      await repositoryService.getRepositories(user, organizationName);
+      await repositoryService.getRepositories(user);
 
       // Then
       expect(spy).toBeCalledTimes(1);
-      expect(spy).toBeCalledWith(user, organizationName);
+      expect(spy).toBeCalledWith(user);
     });
   });
 });
