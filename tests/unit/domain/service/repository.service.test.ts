0f4dfea13bf7ce53a76991c5e1690e1589bfef23
pierre.oucif <pierre.oucif@gadz.org>
2023-02-21 06:34:55 +0100

    Adding my first UT

--- tests/unit/domain/service/repository.service.test.ts
+++ tests/unit/domain/service/repository.service.test.ts
@@ -5,6 +5,7 @@ import { faker } from '@faker-js/faker';
 import { VCSProvider } from 'src/domain/model/vcs-provider.enum';
 import { RepositoryService } from 'src/domain/service/repository.service';
 import ConfigurationStoragePort from 'src/domain/port/out/configuration.storage.port';
+import { VcsRepository } from '../../../../src/domain/model/vcs.repository.model';
 
 describe('RepositoryService', () => {
   describe('getRepositories', () => {
@@ -39,6 +40,52 @@ describe('RepositoryService', () => {
       // Then
       expect(spy).toBeCalledTimes(1);
       expect(spy).toBeCalledWith(user);
+    });
+  });
+
+  describe('getRepositoryById', () => {
+    it('should get a repository given a vcsId', async () => {
+      // Given
+      const user: User = new User(
+        faker.datatype.uuid(),
+        faker.internet.email(),
+        VCSProvider.GitHub,
+        faker.datatype.number(),
+      );
+      const repositoryVcsId: number = Number.parseInt(faker.random.numeric(2));
+      const githubAdapterPort: GithubAdapterPort = mock<GithubAdapterPort>();
+      const configurationStoragePort: ConfigurationStoragePort =
+        mock<ConfigurationStoragePort>();
+      const repositoryService: RepositoryService = new RepositoryService(
+        githubAdapterPort,
+        configurationStoragePort,
+      );
+      const expectedVcsRepository = new VcsRepository(
+        1,
+        faker.name.firstName(),
+        {
+          name: faker.name.lastName(),
+          id: 2,
+          avatarUrl: faker.internet.url(),
+        },
+        new Date(),
+        VCSProvider.GitHub,
+        faker.internet.url(),
+      );
+
+      // When
+      const getRepositoryByIdMock = jest
+        .spyOn(githubAdapterPort, 'getRepositoryById')
+        .mockImplementation(() => Promise.resolve(expectedVcsRepository));
+      const vcsRepositoryById = await repositoryService.getRepositoryById(
+        user,
+        repositoryVcsId,
+      );
+
+      // Then
+      expect(getRepositoryByIdMock).toBeCalledTimes(1);
+      expect(getRepositoryByIdMock).toBeCalledWith(user, repositoryVcsId);
+      expect(vcsRepositoryById).toEqual(expectedVcsRepository);
     });
   });
 });
