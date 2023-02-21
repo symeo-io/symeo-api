86e2591ddbba30865515b2a7aaf9ac5b57913227
Georges Biaux <georges.biaux@symeo.io>
2023-02-21 07:28:55 +0100

    Improve cache

--- src/infrastructure/auth0-adapter/adapter/vcs-access-token.adapter.ts
+++ src/infrastructure/auth0-adapter/adapter/vcs-access-token.adapter.ts
@@ -1,27 +1,31 @@
 import VCSAccessTokenStorage from 'src/domain/port/out/vcs-access-token.storage';
 import { Auth0Client } from 'src/infrastructure/auth0-adapter/auth0.client';
 import User from 'src/domain/model/user.model';
+import { User as Auth0User } from 'auth0';
 
 export class VCSAccessTokenAdapter implements VCSAccessTokenStorage {
-  private cache: Record<string, Record<string, string | undefined>> = {};
+  private cache: Record<
+    string,
+    Record<string, Promise<Auth0User> | undefined>
+  > = {};
   constructor(private auth0Client: Auth0Client) {}
 
   async getGitHubAccessToken(user: User): Promise<string | undefined> {
     if (
-      this.cache[user.id] &&
-      this.cache[user.id][user.accessTokenExpiration]
+      !this.cache[user.id] ||
+      !this.cache[user.id][user.accessTokenExpiration]
     ) {
-      return this.cache[user.id][user.accessTokenExpiration];
+      this.cache[user.id] = {
+        [user.accessTokenExpiration]: this.auth0Client.client.getUser({
+          id: user.id,
+        }),
+      };
     }
 
-    const userData = await this.auth0Client.client.getUser({ id: user.id });
-    const githubIdentity = userData.identities?.find(
+    const userData = await this.cache[user.id][user.accessTokenExpiration];
+    const githubIdentity = userData?.identities?.find(
       (identity) => identity.provider === 'github',
     );
-
-    this.cache[user.id] = {
-      [user.accessTokenExpiration]: githubIdentity?.access_token,
-    };
 
     return githubIdentity?.access_token;
   }
