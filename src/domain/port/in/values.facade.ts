c0f241f72dc1e38abbacd9c58fa46959602f9676
Georges Biaux <georges.biaux@symeo.io>
2023-02-24 15:14:42 +0100

    Migrate values controller

--- src/domain/port/in/values.facade.ts
+++ src/domain/port/in/values.facade.ts
@@ -1,24 +1,13 @@
 import { ConfigurationValues } from 'src/domain/model/configuration/configuration-values.model';
 import User from 'src/domain/model/user/user.model';
 import { VCSProvider } from 'src/domain/model/vcs/vcs-provider.enum';
+import Environment from 'src/domain/model/environment/environment.model';
 
 export interface ValuesFacade {
   findByEnvironmentId(environmentId: string): Promise<ConfigurationValues>;
 
-  findByIdForUser(
-    user: User,
-    vcsType: VCSProvider,
-    repositoryVcsId: number,
-    configurationId: string,
-    environmentId: string,
-  ): Promise<ConfigurationValues>;
-
-  updateByIdForUser(
-    user: User,
-    vcsType: VCSProvider,
-    repositoryVcsId: number,
-    configurationId: string,
-    environmentId: string,
+  updateByEnvironment(
+    environment: Environment,
     values: ConfigurationValues,
   ): Promise<void>;
 }
