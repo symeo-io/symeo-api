6760bbd08f9dbc6f1331e51cceee19e107c77567
Georges Biaux <georges.biaux@symeo.io>
2023-03-31 10:09:14 +0200

    Fix migration

--- src/infrastructure/postgres-adapter/migrations/1680247712034-rollback-audit.ts
+++ src/infrastructure/postgres-adapter/migrations/1680247712034-rollback-audit.ts
@@ -3,7 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class rollbackAudit1680247712034 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'ALTER TYPE "public"."environment_audit_event_type_enum" ADD VALUE IF NOT EXISTS \'versionRollback\'',
+      `DROP TYPE "public"."environment_audit_event_type_enum"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."environment_audit_event_type_enum" AS ENUM('created','updated','deleted','permissionUpdated', 'apiKeyCreated', 'apiKeyDeleted', 'valuesUpdated', 'secretsRead', 'versionRollback')`,
     );
   }
 
