6d7547b45f69c6a9adece91f2015654400014d88
Dorian Frances <dfrances.pro@gmail.com>
2023-03-31 11:12:53 +0200

    migration fix

--- src/infrastructure/postgres-adapter/migrations/1680082271003-addGitlabToConfigurationVcsTypeEnum.ts
+++ src/infrastructure/postgres-adapter/migrations/1680082271003-addGitlabToConfigurationVcsTypeEnum.ts
@@ -5,14 +5,39 @@ export class addGitlabToConfigurationVcsTypeEnum1680082271003
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'ALTER TYPE "public"."configurations_vcstype_enum" ADD VALUE IF NOT EXISTS \'gitlab\'',
+      `ALTER TABLE "configurations" ALTER COLUMN "vcsType" TYPE VARCHAR(255)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "analytics-sdk-values-read" ALTER COLUMN "vcsType" TYPE VARCHAR(255)`,
+    );
+    await queryRunner.query(`DROP TYPE configurations_vcstype_enum`);
+    await queryRunner.query(
+      `CREATE TYPE configurations_vcstype_enum AS ENUM('github', 'gitlab)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "configurations" ALTER COLUMN "vcsType" TYPE configurations_vcstype_enum USING ("vcsType"::configurations_vcstype_enum)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "analytics-sdk-values-read" ALTER COLUMN "vcsType" TYPE configurations_vcstype_enum USING ("vcsType"::configurations_vcstype_enum)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TYPE "public"."configurations_vcstype_enum"`);
     await queryRunner.query(
-      `CREATE TYPE "public"."configurations_vcstype_enum" AS ENUM('github')`,
+      `ALTER TABLE "configurations" ALTER COLUMN "vcsType" TYPE VARCHAR(255)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "analytics-sdk-values-read" ALTER COLUMN "vcsType" TYPE VARCHAR(255)`,
+    );
+    await queryRunner.query(`DROP TYPE configurations_vcstype_enum`);
+    await queryRunner.query(
+      `CREATE TYPE configurations_vcstype_enum AS ENUM('github')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "configurations" ALTER COLUMN "vcsType" TYPE configurations_vcstype_enum USING ("vcsType"::configurations_vcstype_enum)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "analytics-sdk-values-read" ALTER COLUMN "vcsType" TYPE configurations_vcstype_enum USING ("vcsType"::configurations_vcstype_enum)`,
     );
   }
 }
