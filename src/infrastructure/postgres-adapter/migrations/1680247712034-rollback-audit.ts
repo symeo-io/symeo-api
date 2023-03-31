baf316cae160ca68d921c4ac06347ba4b9eef563
Georges Biaux <georges.biaux@symeo.io>
2023-03-31 10:52:30 +0200

    Fix migration

--- src/infrastructure/postgres-adapter/migrations/1680247712034-rollback-audit.ts
+++ src/infrastructure/postgres-adapter/migrations/1680247712034-rollback-audit.ts
@@ -3,31 +3,27 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class rollbackAudit1680247712034 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "public"."environment-audits" ALTER COLUMN "eventType" TYPE VARCHAR(255)`,
+      `ALTER TABLE "environment-audits" ALTER COLUMN "eventType" TYPE VARCHAR(255)`,
+    );
+    await queryRunner.query(`DROP TYPE environment_audit_event_type_enum`);
+    await queryRunner.query(
+      `CREATE TYPE environment_audit_event_type_enum AS ENUM('created','updated','deleted','permissionUpdated', 'apiKeyCreated', 'apiKeyDeleted', 'valuesUpdated', 'secretsRead', 'versionRollback')`,
     );
     await queryRunner.query(
-      `DROP TYPE "public"."environment_audit_event_type_enum"`,
-    );
-    await queryRunner.query(
-      `CREATE TYPE "public"."environment_audit_event_type_enum" AS ENUM('created','updated','deleted','permissionUpdated', 'apiKeyCreated', 'apiKeyDeleted', 'valuesUpdated', 'secretsRead', 'versionRollback')`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "public"."environment-audits" ALTER COLUMN column_name TYPE "public"."environment_audit_event_type_enum" USING ("public"."environment-audits"::"public"."environment_audit_event_type_enum")`,
+      `ALTER TABLE "environment-audits" ALTER COLUMN "eventType" TYPE environment_audit_event_type_enum USING ("eventType"::environment_audit_event_type_enum)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "public"."environment-audits" ALTER COLUMN "eventType" TYPE VARCHAR(255)`,
+      `ALTER TABLE "environment-audits" ALTER COLUMN "eventType" TYPE VARCHAR(255)`,
+    );
+    await queryRunner.query(`DROP TYPE environment_audit_event_type_enum`);
+    await queryRunner.query(
+      `CREATE TYPE environment_audit_event_type_enum AS ENUM('created','updated','deleted','permissionUpdated', 'apiKeyCreated', 'apiKeyDeleted', 'valuesUpdated', 'secretsRead')`,
     );
     await queryRunner.query(
-      `DROP TYPE "public"."environment_audit_event_type_enum"`,
-    );
-    await queryRunner.query(
-      `CREATE TYPE "public"."environment_audit_event_type_enum" AS ENUM('created','updated','deleted','permissionUpdated', 'apiKeyCreated', 'apiKeyDeleted', 'valuesUpdated', 'secretsRead')`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "public"."environment-audits" ALTER COLUMN column_name TYPE "public"."environment_audit_event_type_enum" USING ("public"."environment-audits"::"public"."environment_audit_event_type_enum")`,
+      `ALTER TABLE "environment-audits" ALTER COLUMN "eventType" TYPE environment_audit_event_type_enum USING ("eventType"::"environment_audit_event_type_enum")`,
     );
   }
 }
